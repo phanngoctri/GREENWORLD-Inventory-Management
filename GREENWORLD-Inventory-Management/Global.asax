67a9a418a09cbdb17176436a625cf95560652920
Tri Phan Ngoc <43377863+phanngoctri@users.noreply.github.com>
2021-07-23 15:55:15 +0700

    Add project files.

---
+++ GREENWORLD-Inventory-Management/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GREENWORLD_Inventory_Management.MvcApplication" Language="C#" %>
---
+++ GREENWORLD-Inventory-Management/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GREENWORLD_Inventory_Management.Startup))]
+namespace GREENWORLD_Inventory_Management
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
